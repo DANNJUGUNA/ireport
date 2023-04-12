3d354783bf3c27ccb6fce7c6d8b501d36c8e42d6
DANNJUGUNA <ndan30071@gmail.com>
2023-04-12 19:46:21 +0300

    created the back end

---
+++ backend/vendor/bundle/ruby/3.0.0/gems/irb-1.6.4/lib/irb/version.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: false
+#
+#   irb/version.rb - irb version definition file
+#   	by Keiju ISHITSUKA(keiju@ishitsuka.com)
+#
+
+module IRB # :nodoc:
+  VERSION = "1.6.4"
+  @RELEASE_VERSION = VERSION
+  @LAST_UPDATE_DATE = "2023-04-07"
+end
---
+++ backend/vendor/bundle/ruby/3.0.0/gems/reline-0.3.3/lib/reline/version.rb
@@ -0,0 +1,3 @@
+module Reline
+  VERSION = '0.3.3'
+end
