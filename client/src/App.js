1570b0618bffa3441456b674b74e54d802e6b565
irriz-jayy <jayfelix789@gmail.com>
2023-04-16 12:20:04 +0300

    Created footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Footer />
     </div>
   );
-
 }
 
 export default App;
