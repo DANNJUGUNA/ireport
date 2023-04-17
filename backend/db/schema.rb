d6818e7f2a65b45e6cb6bfd23c06402768c062b5
Felix <felix.irungu@student.moringaschool.com>
2023-04-17 15:52:28 +0300

    report model done

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.0].define(version: 2023_04_17_075743) do
-=======
-ActiveRecord::Schema[7.0].define(version: 2023_04_16_153410) do
->>>>>>> master
+ActiveRecord::Schema[7.0].define(version: 2023_04_17_123740) do
   create_table "admins", force: :cascade do |t|
     t.string "first_name"
     t.string "surname"
@@ -24,7 +20,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_16_153410) do
     t.datetime "updated_at", null: false
   end
 
-<<<<<<< HEAD
   create_table "report_statuses", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -38,12 +33,20 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_16_153410) do
   end
 
   create_table "reports", force: :cascade do |t|
+    t.string "description"
+    t.string "image"
+    t.string "video"
+    t.string "gps_coordinates"
+    t.integer "user_id", null: false
+    t.integer "report_status_id", null: false
+    t.integer "report_type_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["report_status_id"], name: "index_reports_on_report_status_id"
+    t.index ["report_type_id"], name: "index_reports_on_report_type_id"
+    t.index ["user_id"], name: "index_reports_on_user_id"
   end
 
-=======
->>>>>>> master
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "surname"
@@ -53,4 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_16_153410) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "reports", "report_statuses"
+  add_foreign_key "reports", "report_types"
+  add_foreign_key "reports", "users"
 end
