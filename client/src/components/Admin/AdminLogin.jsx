8e44c9612b73ff59b34dd4a0231d8192b573486b
esther <esther.githinji@student.moringaschool.com>
2023-04-17 13:22:22 +0300

    changed button background color

--- client/src/components/Admin/AdminLogin.jsx
+++ client/src/components/Admin/AdminLogin.jsx
@@ -55,7 +55,7 @@ function AdminLogin() {
                     <div>
                         <button
                         type="submit"
-                        className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                        className="flex w-full justify-center rounded-md border border-transparent bg-main1 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                         >
                         Sign in
                         </button>
