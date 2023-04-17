2542ea2383f78f8271ebf94c1493077d242e7085
irriz-jayy <jayfelix789@gmail.com>
2023-04-17 12:29:24 +0300

    Resized images and added logo to navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -47,12 +47,12 @@ export default function Navbar() {
                 <div className="flex flex-shrink-0 items-center">
                   <img
                     className="block h-8 w-auto lg:hidden"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                    src="https://bit.ly/3UDPP5e"
                     alt="Your Company"
                   />
                   <img
                     className="hidden h-8 w-auto lg:block"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                    src="https://bit.ly/3GL4ZQk"
                     alt="Your Company"
                   />
                 </div>
