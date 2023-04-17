29794a77f679cf90cfd21c55e09997434c1100ee
Felix <felix.irungu@student.moringaschool.com>
2023-04-17 13:21:20 +0300

    report model added

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :reports
   resources :admins
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
