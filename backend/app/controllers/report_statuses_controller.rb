e57ee8c9d6b9746a26b20daaf30e1e73308488b8
MIKEL DAVIS <kipkorir385@gmail.com>
2023-04-18 15:42:02 +0300

    update-status  functionality

--- backend/app/controllers/report_statuses_controller.rb
+++ backend/app/controllers/report_statuses_controller.rb
@@ -2,5 +2,13 @@ class ReportStatusesController < ApplicationController
     def show
     render json: report_status
   end
+
+  def update
+    if report_status.update(report_status_params)
+      render json: report_status
+    else
+      render json: report_status.errors, status: :unprocessable_entity
+    end
+  end
   
 end
