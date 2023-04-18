fbcf544af26e5b0be26f16f0361bc340f55025a2
Felix <felix.irungu@student.moringaschool.com>
2023-04-18 18:06:11 +0300

    Report endpoints added

--- backend/app/controllers/reports_controller.rb
+++ backend/app/controllers/reports_controller.rb
@@ -1,46 +1,49 @@
 class ReportsController < ApplicationController
-    before_action :set_report, only: [:show, :update, :destroy]
-    before_action :authorize_request
-  
-    def index
-      @reports = Report.all
-      render json: { reports: @reports }
-    end
-  
-    def show
-      render json: { report: @report }
-    end
-  
-    def create
-      @report = @current_user.reports.build(report_params)
-      if @report.save
-        render json: { report: @report }, status: :created
-      else
-        render json: { errors: @report.errors.full_messages }, status: :unprocessable_entity
-      end
-    end
-  
-    def update
-      if @report.update(report_params)
-        render json: { report: @report }
-      else
-        render json: { errors: @report.errors.full_messages }, status: :unprocessable_entity
-      end
-    end
-  
-    def destroy
-      @report.destroy
-      head :no_content
-    end
-  
-    private
-  
-    def set_report
-      @report = Report.find(params[:id])
-    end
-  
-    def report_params
-      params.require(:report).permit(:description, :image, :video, :gps_coordinates, :report_type_id, :status_type_id)
+  before_action :set_report, only: [:show, :update, :destroy]
+
+  # GET /reports
+  def index
+    @reports = Report.all
+    render json: @reports
+  end
+
+  # GET /reports/:id
+  def show
+    render json: @report
+  end
+
+  # POST /reports
+  def create
+    @report = Report.new(report_params)
+    if @report.save
+      render json: @report, status: :created
+    else
+      render json: @report.errors, status: :unprocessable_entity
     end
   end
-  
+
+  # PATCH/PUT /reports/:id
+  def update
+    if @report.update(report_params)
+      render json: @report
+    else
+      render json: @report.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /reports/:id
+  def destroy
+    @report.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_report
+    @report = Report.find(params[:id])
+  end
+
+  def report_params
+    params.require(:report).permit(:description, :image, :video, :gps_coordinates, :user_id, :report_status_id, :report_type_id)
+  end
+end
