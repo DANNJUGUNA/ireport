20db1d22f404588782ae6f5e29b1ccd483300298
DANNJUGUNA <ndan30071@gmail.com>
2023-04-18 15:39:01 +0300

    defined all the actions for the routes

--- backend/app/controllers/admins_controller.rb
+++ backend/app/controllers/admins_controller.rb
@@ -1,2 +1,18 @@
 class AdminsController < ApplicationController
+    def index
+        admin=Admin.all 
+        render json: admin, status: :ok
+    end
+    def show
+        
+    end
+    def create
+        
+    end
+    def update
+        
+    end
+    def destroy
+        
+    end
 end
