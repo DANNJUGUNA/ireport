4977609f7d8cc16a9ef70078b79e7d7409187125
MIKEL DAVIS <kipkorir385@gmail.com>
2023-04-18 17:06:47 +0300

    Create report type

--- backend/app/controllers/report_types_controller.rb
+++ backend/app/controllers/report_types_controller.rb
@@ -1,4 +1,11 @@
 class ReportTypesController < ApplicationController
-  
+   def create
+    @report_type = ReportType.new(report_type_params)
+    if @report_type.save
+      redirect_to @report_type
+    else
+      render :new
+    end
+  end
 
 end
