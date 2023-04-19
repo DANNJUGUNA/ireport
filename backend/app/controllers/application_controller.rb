d44d2ceb0bb9de7a294dd00e8a1a44c4a207f315
DANNJUGUNA <ndan30071@gmail.com>
2023-04-19 12:29:56 +0300

    add encode_token method to encode jwt

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::API
     rescue_from ActiveRecord::RecordInvalid, with: :validation_errors
     rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
+    def encode_token(payload)
+        JWT.encode(payload,"secret")
+    end
     private
 
     def validation_errors(invalid)
