9e280f4362992f061494f0ba2111a70902aa1767
DANNJUGUNA <ndan30071@gmail.com>
2023-04-20 07:46:33 +0300

    added the login function

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
 import { useContext,useState,createContext,useEffect } from 'react'
-export const AuthContext = () => {
-  return (
-    <div>AuthContext</div>
-  )
-}
+function loginUser(email, password) {
+    return fetch('/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ email: email, password: password })
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data.authorized) {
+        const token = data.token;
+  
+        // save the token in localStorage or sessionStorage for future use
+        return { user: data.user, token: token };
+      } else {
+        throw new Error('Invalid email or password');
+      }
+    });
+  }
+export const AuthContext =createContext({
+  user:nill,
+  token:nill
+
+})
