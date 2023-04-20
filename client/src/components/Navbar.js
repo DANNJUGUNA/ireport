e56a3088f4fb503eb4b5e02fb0eab2a7ab3c1f3c
DANNJUGUNA <ndan30071@gmail.com>
2023-04-20 07:40:02 +0300

    made the login button to navigate to the lon page and signup to navigate to signup

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -83,20 +83,20 @@ export default function Navbar() {
               </div>
               <div className="flex items-center space-x-4">
                 <div className="flex-shrink-0">
-                  <button
+                  <a href="/login"
                     type="button"
                     className="relative inline-flex items-center rounded-md border border-transparent bg-button px-4 py-2 text-sm font-medium text-primary shadow-sm hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-800"
                   >
                     <span>Login</span>
-                  </button>
+                  </a>
                 </div>
                 <div className="flex-shrink-0">
-                  <button
+                  <a href="/signup"
                     type="button"
                     className="relative inline-flex items-center rounded-md border border-transparent bg-button px-4 py-2 text-sm font-medium text-primary shadow-sm hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-800"
                   >
                     <span>Sign up</span>
-                  </button>
+                  </a>
                 </div>
                 <div className="hidden md:ml-4 md:flex md:flex-shrink-0 md:items-center">
                   {/* Profile dropdown */}
