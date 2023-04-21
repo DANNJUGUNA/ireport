a1755d50c7c161cfa2439a9873aa09211f43dfde
DANNJUGUNA <ndan30071@gmail.com>
2023-04-21 10:53:32 +0300

    updated the user login function

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -5,25 +5,25 @@ import Swal from 'sweetalert2';
 
 
 function loginUser(email, password) {
-    return fetch('/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ email: email, password: password })
-    })
-    .then(response => response.json())
-    .then(data => {
-      if (data.authorized) {
-        const token = data.token;
-  
-        // save the token in localStorage or sessionStorage for future use
-        return { user: data.user, token: token };
-      } else {
-        throw new Error('Invalid email or password');
-      }
-    });
-  }
+  return fetch('/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ email: email, password: password })
+  })
+  .then(response => response.json())
+  .then(data => {
+    if (data.authorized) {
+      const token = data.token;
+       console.log(token)
+      // save the token in localStorage or sessionStorage for future use
+      return { user: data.user, token: token };
+    } else {
+      throw new Error('Invalid username or password');
+    }
+  });
+}
 export const AuthContext =createContext({
     user: null,
     token: null,
