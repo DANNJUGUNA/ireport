5c04c1abcb226cbe4432493e3f855f525b3ffa18
DANNJUGUNA <ndan30071@gmail.com>
2023-04-22 18:00:59 +0300

    Revert "collected the login error on the backend"
    This reverts commit 9329f7a671805891ce8cab61e4ad079be04e56f8.

--- backend/app/controllers/reports_controller.rb
+++ backend/app/controllers/reports_controller.rb
@@ -1,5 +1,5 @@
 class ReportsController < ApplicationController
-  before_action :authorized
+  skip_before_action :authorized, only: [:index, :show, :create]
 
   # GET /reports.
   def index
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   resources :users ,only: [ :index,:show]
   post'/login', to: "users#login"
   post'/signup',to:"users#signup"
-  delete'/logout',to:"users#logout"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
