59056cc267b229882b432035ff728cf7c557fd34
Collins Chirchir <collinskchirchir@gmail.com>
2023-04-22 17:57:59 +0300

    index report_statuses

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :reports
   resources :admins
   resources :users ,only: [ :index,:show]
+  resources :report_statuses, only: [:index]
   post'/login', to: "users#login"
   post'/signup',to:"users#signup"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
