6d151d682216b549109f0f2d71bb71e208b38a85
Collins Chirchir <collinskchirchir@gmail.com>
2023-04-22 17:58:33 +0300

    title and desc summary serialization

--- backend/app/serializers/report_serializer.rb
+++ backend/app/serializers/report_serializer.rb
@@ -1,5 +1,13 @@
 class ReportSerializer < ActiveModel::Serializer
-  attributes :id,:description,:image,:video,:gps_coordinates, :title, :location_name, :created_at, :updated_at
+  attributes :id,:description,:image,:video,:gps_coordinates, :title, :location_name, :created_at, :updated_at, :description_summary, :title_summary
+
+  def description_summary
+    "#{self.object.description[0..25]}..."
+  end
+  def title_summary
+    "#{self.object.title[0..25]}..."
+  end
+
   belongs_to :user
   belongs_to :report_status
   belongs_to :report_type
