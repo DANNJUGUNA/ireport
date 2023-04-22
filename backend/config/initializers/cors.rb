9c47828bd6e50d32e224b1a70d12509c37539d05
DANNJUGUNA <ndan30071@gmail.com>
2023-04-22 18:00:43 +0300

    Revert "updated the origin"
    This reverts commit 0a2f0b784dcb91b6bbc51dfaa8afda7240515e51.

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:4000"
+    origins "example.com"
 
     resource "*",
       headers: :any,
