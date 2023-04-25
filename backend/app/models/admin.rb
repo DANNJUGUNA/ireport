b73fbae2bd3669f31dba19d1a990716696dd2488
DANNJUGUNA <ndan30071@gmail.com>
2023-04-25 15:41:26 +0300

    added the secure password functionality

--- backend/app/models/admin.rb
+++ backend/app/models/admin.rb
@@ -1,4 +1,5 @@
 class Admin < ApplicationRecord
+    has_secure_password
     validates :email, presence: true, uniqueness:  true
     validates :first_name, presence: true
     validates :surname,presence: true
