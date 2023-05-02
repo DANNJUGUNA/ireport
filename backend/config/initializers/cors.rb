1412f70554493bffe99d5f11d87ce0a9c31fcc9d
DANNJUGUNA <ndan30071@gmail.com>
2023-05-02 11:18:44 +0300

    made changes

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -5,14 +5,20 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+
+#     origins "https://ireport-eight.vercel.app/"
+
+#     resource "*",
+#       headers: :any,
+#        methods: [:get, :post, :put, :patch, :delete, :options, :head]
+#    end
+#  end
+
+ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-
     origins "https://ireport-eight.vercel.app"
-
-    resource "*",
-      headers: :any,
-       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-   end
- end
-
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true, expose: ['Access-Control-Allow-Origin']
+  end
+end
